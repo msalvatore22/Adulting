6583614ae4730fc93bf65ba0f247714957975e6b
Michael Salvatore <msalvatore22@gmail.com>
2018-05-29 15:32:44 -0400

    built the model, view and controller for likes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'likes/index'
-  get 'likes/show'
-  get 'likes/new'
-  get 'likes/edit'
-  get 'likes/create'
-  get 'likes/update'
-  get 'likes/destroy'
+  
   get "home/index"
   root "home#index"
 
+  reources :likes
   resources :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_29_150521) do
+ActiveRecord::Schema.define(version: 2018_05_29_180919) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
