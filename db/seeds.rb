0a49441bab3651b74446c6283d2275b715652679
Maria108 <maria.abash@gmail.com>
2018-05-29 16:52:36 -0400

    Setup Profiles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Profile.create(
+  first_name: "Cobra",
+  last_name: "Good",
+  username: "cobra",
+).build_user(email: "cobra@cobra.com", password: "goodcobra").save
