ec9b4c19a1834a76506a568eb3e9ab2a82d6364c
Maria108 <maria.abash@gmail.com>
2018-05-31 14:21:04 -0400

    Show users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,17 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  helper_method :redirect_back
 
+  def redirect_back(options = {})
+    if request.referer
+      redirect_to request.referer, options
+    else
+      redirect_to root_path, options
+    end
+  end
+
+  protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,5 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    unless @user == current_user
-      redirect_to :back, :alert => "Access denied."
-    end
   end
 end
