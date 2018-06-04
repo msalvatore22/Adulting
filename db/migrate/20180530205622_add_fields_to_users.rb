7fe4e3aaf77ca5e53c058138cfa92804d6f61170
Maria108 <maria.abash@gmail.com>
2018-06-04 13:53:55 -0400

    Default picture for user

--- db/migrate/20180530205622_add_fields_to_users.rb
+++ db/migrate/20180530205622_add_fields_to_users.rb
@@ -3,5 +3,6 @@ class AddFieldsToUsers < ActiveRecord::Migration[5.2]
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
     add_column :users, :username, :string
+    add_column :users, :picture, :string
   end
 end
