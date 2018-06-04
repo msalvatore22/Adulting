1850fb62d26b04da03542d89ded7f653b6147d3b
Tony Mac <tonymac@Tony.local>
2018-06-04 18:44:07 -0400

    topics posts and posts index layout fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,7 @@ class PostsController < ApplicationController
     if params[:search]
       @posts = Post.where('topic LIKE ?', "%#{params[:search]}%")
     else
+      @user = current_user
       @posts = Post.all
     end
   end
@@ -70,27 +71,32 @@ class PostsController < ApplicationController
 
   def car_posts
     @posts = Post.where(topic: 'Cars').all
-
+    @user = current_user
   end
 
   def finance_posts
     @posts = Post.where(topic: 'Finance').all
+    @user = current_user
   end
 
   def housing_posts
     @posts = Post.where(topic: 'Housing').all
+    @user = current_user
   end
 
   def lifestyle_posts
     @posts = Post.where(topic: 'Lifestyle').all
+    @user = current_user
   end
 
   def technology_posts
     @posts = Post.where(topic: 'Technology').all
+    @user = current_user
   end
 
   def travel_posts
     @posts = Post.where(topic: 'Travel').all
+    @user = current_user
   end
 
   private
