bdca3016fcda80be60f0310f85e95c18cd8160df
Michael Salvatore <msalvatore22@gmail.com>
2018-06-06 15:33:08 -0400

    heroku might finally work

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
     if params[:search]
       @posts = Post.where('topic LIKE ?', "%#{params[:search]}%")
     else
-      @user = User.find_by(params[:id])
+      @user = current_user
       @posts = Post.all
     end
   end
